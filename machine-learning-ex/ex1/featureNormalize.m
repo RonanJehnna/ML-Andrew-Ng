4f74b72eaf47f71e24532e439f89e64a1f282acd
Anil Kumar <anilssaharan23@gmail.com>
2021-02-16 02:39:57 +0530

    Linear Regression : Cost Function and Gradient Descent

--- machine-learning-ex/ex1/featureNormalize.m
+++ machine-learning-ex/ex1/featureNormalize.m
@@ -25,9 +25,14 @@ sigma = zeros(1, size(X, 2));
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
+for i = 1:size(X,2)
+    mu(1,i) = mean(X(:,i));
+    sigma(1,i) = std(X(:,i));
+endfor
+for i = 1:size(X,2)
+    X_norm(:,i) = (X(:,i) - mu(1,i)*ones(size(X,1),1));
+    X_norm(:,i) = X_norm(:,i) / sigma(1,i);
+endfor
 
 
 
